75a33863d8882447a7e6034d4ac9f33f95f7490d
raxmatillo-ruziyev <raxmatillo248@gmail.com>
2024-07-30 15:24:24 +0500

    add collage component all page

--- src/Pages/Zomin/Zomin.jsx
+++ src/Pages/Zomin/Zomin.jsx
@@ -14,6 +14,7 @@ import image6 from '../../assets/rasm16.jpeg';
 import image7 from '../../assets/rasm17.jpeg';
 import image8 from '../../assets/rasm18.jpeg';
 import image9 from '../../assets/rasm10.jpeg';
+import Collage from '../../Components/Collage/Collage'
 const Zomin = () => {
     const {t,i18n} =useTranslation()
   return (
@@ -42,7 +43,9 @@ const Zomin = () => {
           </iframe>
           
         }/>
-        
+         <Collage image1={image1} image2={image2} image3={image3} image4={image4} image5={image5} image6={image6} image7={image7} image8={image8} image9={image9}/>
+ 
+
     </>
   )
 }
