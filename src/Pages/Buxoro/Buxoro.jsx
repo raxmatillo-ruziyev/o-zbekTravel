f3d3a446ba93e1eb0d8299710bc02aebf2ff4e23
raxmatillo-ruziyev <raxmatillo248@gmail.com>
2024-07-30 16:21:56 +0500

    add section component all pages

--- src/Pages/Buxoro/Buxoro.jsx
+++ src/Pages/Buxoro/Buxoro.jsx
@@ -15,6 +15,7 @@ import image7 from '../../assets/rasm17.jpeg';
 import image8 from '../../assets/rasm18.jpeg';
 import image9 from '../../assets/rasm10.jpeg';
 import Collage from '../../Components/Collage/Collage'
+import Section from '../../Components/Section/Section'
 const Buxoro = () => {
     const {t,i18n} =useTranslation()
   return (
@@ -44,6 +45,7 @@ const Buxoro = () => {
           
         }/>
          <Collage image1={image1} image2={image2} image3={image3} image4={image4} image5={image5} image6={image6} image7={image7} image8={image8} image9={image9}/>
+         <Section/>
  
     </>
   )
--- src/Pages/Turkiston/Turkiston.jsx
+++ src/Pages/Turkiston/Turkiston.jsx
@@ -44,6 +44,7 @@ const Turkiston = () => {
           
         }/>
          <Collage image1={image1} image2={image2} image3={image3} image4={image4} image5={image5} image6={image6} image7={image7} image8={image8} image9={image9}/>
+         <Section/>
  
     </>
   )
