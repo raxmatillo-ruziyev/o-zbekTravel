257a7b8697da6bfa2ce1855080583351ae3d17b5
raxmatillo-ruziyev <raxmatillo248@gmail.com>
2024-07-30 12:32:50 +0500

    add hero component

---
+++ src/Components/Hero/Hero.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Hero.scss'
+import { useTranslation } from 'react-i18next'
+const Hero = ({iframe}) => {
+    const {t,i18n} = useTranslation();
+  return (
+    <>
+    <div className="hero">
+        <div className="container">
+            <h1 className="hero-title">{t("22")}</h1>
+            <div>{iframe}</div>
+        </div>
+    </div>
+      
+    </>
+  )
+}
+
+export default Hero
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,23 @@
 import React from 'react'
 import './Home.scss'
 import Header from '../../Components/Header/Header'
+import Hero from '../../Components/Hero/Hero'
 
 const Home = () => {
   return (
     <>
-<Header/>
-      
+      <Header />
+      <Hero iframe={
+   <iframe
+   width="560"
+   height="315"
+   src="https://www.youtube.com/embed/48PSJqwZVIc?si=m7yM65XklCITGD09&controls=0&showinfo=0&modestbranding=1&rel=0"
+   title="YouTube video player"
+   allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+   referrerPolicy="strict-origin-when-cross-origin"
+   allowFullScreen
+ ></iframe>}/>
+ 
     </>
   )
 }
