31ad1a1631e22c9ab01239939c8e968e56421825
raxmatillo-ruziyev <raxmatillo248@gmail.com>
2024-07-30 16:19:39 +0500

    add section component

---
+++ src/Components/Section/Section.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import './Section.scss'
+import { useTranslation } from 'react-i18next'
+import Box from '../Box/Box';
+import Button from '../Button/Button';
+const Section = () => {
+    const { t, i18n } = useTranslation();
+    return (
+        <>
+            <div className="section">
+                <div className="container">
+                    <div className="section-box">
+                        <h1 className="section-title">{t("26")}</h1>
+                        <p className="section-text">{t("27")}</p>
+                        <ul className="section-list">
+                            <li className="section-item"><Box boxTitle={t("11")} boxText={t("12")} /></li>
+                            <li className="section-item"><Box boxTitle={t("13")} boxText={t("14")} /></li>
+                            <li className="section-item"><Box boxTitle={t("15")} boxText={t("16")} /></li>
+                            <li className="section-item"><Box boxTitle={t("28")} boxText={t("16")} /></li>
+                            <li className="section-item"><Box boxTitle={t("11")} boxText={t("12")} /></li>
+                            <li className="section-item"><Box boxTitle={t("13")} boxText={t("14")} /></li>
+                            <li className="section-item"><Box boxTitle={t("15")} boxText={t("16")} /></li>
+                            <li className="section-item"><Box boxTitle={t("28")} boxText={t("16")} /></li>
+
+                        </ul>
+                        <a className="header-next-item"><Button name={t("29")} color={"#5EC2EC"}/></a>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Section
--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -12,6 +12,7 @@ import image6 from '../../assets/rasm16.jpeg';
 import image7 from '../../assets/rasm17.jpeg';
 import image8 from '../../assets/rasm18.jpeg';
 import image9 from '../../assets/rasm10.jpeg';
+import Section from '../../Components/Section/Section'
 
 const Home = () => {
   return (
@@ -29,7 +30,7 @@ const Home = () => {
    allowFullScreen
  ></iframe>}/>
  <Collage image1={image1} image2={image2} image3={image3} image4={image4} image5={image5} image6={image6} image7={image7} image8={image8} image9={image9}/>
- 
+ <Section/>
     </>
   )
 }
